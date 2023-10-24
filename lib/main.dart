68c9142b06d383a7e95c3b533b5db792ed512371
MarkRywell <gopro855@gmail.com>
2023-10-24 09:37:19 +0800

    refactor: :recycle: Initialize flutter env

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:fleet_management/Credentials/login.dart';
 import 'package:fleet_management/Pages/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main(){
+void main() async {
+
+  await dotenv.load(fileName: ".env");
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'Fleet Management System',
