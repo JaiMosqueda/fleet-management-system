2b6ad7b20bb7c19b2341e91fc50bd55940ab131a
JaiMosqueda <jirahleilmosqueda@gmail.com>
2023-12-09 12:49:42 +0800

    Adding schedule page

--- lib/Pages/schedule_page.dart
+++ lib/Pages/schedule_page.dart
@@ -10,8 +10,127 @@ class SchedulePage extends StatefulWidget {
 class _SchedulePageState extends State<SchedulePage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Schedule'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Delivery'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Form(
+            child: Column(
+              children: <Widget>[
+                Text('LTS | LOLONG TRUCKING SERVICES'),
+                Text('Has No.'),
+                Text('TRUCKER INFORMATION'),
+                Row(
+                  children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Trucker Name:'),
+                    )),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Trucker ID:'),
+                    )),
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Plate No.:'),
+                    )),
+                  ],
+                ),
+                    Row(
+                      children: <Widget>[
+                        Flexible(child: TextFormField(
+                          decoration: InputDecoration(hintText: 'Driver:'),
+                        )),
+                        Flexible(child: TextFormField(
+                          decoration: InputDecoration(hintText: 'Helper:'),
+                        )),
+                  ],
+                ),
+                Text('DELIVERY TO'),
+                Row(
+                  children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Business Name:'),
+                    )),
+                  ],
+                ),
+                    Row(
+                      children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Delivery Address:'),
+                    )),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Contact Person:'),
+                    )),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Contact No.:'),
+                    )),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Delivery Date:'),
+                    )),
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Delivery Time:'),
+                    )),
+                  ],
+                ),
+                Text('DISPATCH INFORMATION'),
+                Row(
+                  children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Dispatch By:'),
+                    )),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Dispatch Date:'),
+                    )),
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Dispatch Time:'),
+                    )),
+                  ],
+                ),
+                Text('DELIVERY ACKNOWLEDGEMENT'),
+                Row(
+                  children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Received By:'),
+                    )),
+                  ],
+                ),
+                Row(
+                  children: <Widget>[
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Received Date:'),
+                    )),
+                    Flexible(child: TextFormField(
+                      decoration: InputDecoration(hintText: 'Received Time:'),
+                    )),
+                  ],
+                ),
+                ElevatedButton(onPressed: (){
+                }, child: Text('Submit'))
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fleet_management/Credentials/login.dart';
-import 'package:fleet_management/Pages/profile_page.dart';
+import 'package:fleet_management/Home/home_page.dart';
+import 'package:fleet_management/Pages/schedule_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -14,6 +15,6 @@ void main() async {
       primarySwatch: Colors.lightGreen,
       scaffoldBackgroundColor: Colors.green
     ),
-    home: ProfilePage(),
+    home: SchedulePage(),
   ));
 }
